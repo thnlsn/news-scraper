b611166a6faae791b66a4731018db52b6e548b2a
Thomas Nelson <thnlsn@gmail.com>
2019-12-06 19:58:51 -0800

    created main.handlebars

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
 
-let PORT = process.env.PORT || 8000;
+let PORT = process.env.PORT || 8080;
 
-app.engine('handlebars', exphbs());
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 // Routing
