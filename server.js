c00062dd1da9485eb733a855409a9425544cb93d
Thomas Nelson <thnlsn@gmail.com>
2019-12-06 20:06:10 -0800

    installed path, set the main layout folder, removed boilerplate from index and about pages

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
 const exphbs = require('express-handlebars');
+const path = require('path');
 
 let PORT = process.env.PORT || 8080;
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine(
+  'handlebars',
+  exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/boilerplate')
+  })
+);
 app.set('view engine', 'handlebars');
 
 // Routing
 app.get('/', (req, res) => {
   res.render('index', { layout: false });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', { layout: false });
 });
 
 app.listen(PORT, () =>
